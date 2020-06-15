4e8bc0a305e45245eb2ac916a2ae52b6ed5c27e5
Nafizul Islam <65622090+172452535@users.noreply.github.com>
2020-06-15 10:38:06 +0600

    Create 10.c

---
+++ lab2/10.c
@@ -0,0 +1,47 @@
+/* Delete a value “X” (take input X from user) from the array shift all other
+value to left.If “X” is not present in the given array print Element Not Found.
+Only delete the first occurrence of the value. And print the whole array */
+
+#include<stdio.h>
+int main ()
+{
+    int array[100]= {1,4,3,4,5,6,4,8,9,10};
+    int i, j, X;
+    int tag=0;
+    int size=10;
+
+    for(i=0; i<size; i++)
+        printf("%d ",array[i]);
+
+    printf("\n");
+
+//Insert Value
+    scanf("%d",&X);
+
+    for(i=0; i<size; i++)
+    {
+        if(X==array[i])
+        {
+            for(j=i; j<=size; j++)
+            {
+                array[j] = array[j+1];
+            }
+            tag++;
+            break;
+        }
+    }
+
+    size--;
+
+    if(tag!=1)
+    {
+        printf("Not Found");
+    }
+    else
+    {
+        for(i=0; i<size; i++)
+            printf("%d ",array[i]);
+    }
+
+    return 0;
+}
