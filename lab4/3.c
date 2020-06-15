4871b7d4112ffcd85edaf7df270f188cbb51156d
Nafizul Islam <65622090+172452535@users.noreply.github.com>
2020-06-15 10:54:07 +0600

    Create 3.c

---
+++ lab4/3.c
@@ -0,0 +1,25 @@
+/* Reverse the given array within the same array */
+
+#include <stdio.h>
+int main()
+{
+    int i, j, tag;
+    int array[100] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    int size = 10;
+
+    for(i=0; i<size; i++)
+        printf("%d ",array[i]);
+
+    printf("\n");
+
+    for(i=size-1, j=0; j<i; i--, j++){
+        tag = array[j];
+        array[j] = array[i];
+        array[i] = tag;
+    }
+
+    for(i=0; i<size; i++)
+        printf("%d ", array[i]);
+
+    return 0;
+}
